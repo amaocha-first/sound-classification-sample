313f7d22ef31d4f32ed4702b208083eef8067ce7
shota-nishizawa <guiltaugusthird03@gmail.com>
2022-05-28 18:00:10 +0900

    Initial Commit

---
+++ sound-analysis-sample/sound_analysis_sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  sound_analysis_sampleApp.swift
+//  sound-analysis-sample
+//
+//  Created by shota-nishizawa on 2022/05/28.
+//
+
+import SwiftUI
+
+@main
+struct sound_analysis_sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
